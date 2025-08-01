db508474fe79695ba6bf3b3a1f4fc1a63a590ca5
Tuncay <tunc270@gmail.com>
2025-08-01 10:19:06 +0300

    up

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 //echo getenv('MY_VARIABLE'); // Çıktı: MerhabaDunya
-echo $_SERVER['REQFILE'];
+echo 'REQFILE:' .$_SERVER['REQFILE'];
 echo "<br/>";
-echo $_SERVER['REQURI'];
+// REQURI , root'dan sonra gelen kısım
+echo 'REQURI:' . $_SERVER['REQURI'];
 echo "<br/>";
 echo 'THE REQUEST:' . $_SERVER['THEREQ'];
 echo "<br/>";
 echo 'TEMP VAR:' . $_SERVER['TEMPVAR'];
+#echo "<br/>";
+#echo 'VAR2:' . $_SERVER['VAR2'];
+echo 'PATH INFO:' . $_SERVER['PATH_INFO'];
+echo "<br/>";
+
+// Query string
+echo 'QUERY STRING:' . $_SERVER['QUERY_STRING'];
